0ab62b446b569c11014170309b29107ce17847cf
Taylor Yelverton <taylor@synergy-solutions.biz>
2012-04-14 16:43:04 -0500

    Im an idiot

--- lib/less_js/source.rb
+++ lib/less_js/source.rb
@@ -1,7 +1,7 @@
 module LessJs
   module Source
     def self.bundled_path
-      if RUBY_PLATFORM ~= /java/
+      if RUBY_PLATFORM =~ /java/
         File.expand_path("../less-rhino.js", __FILE__)
       else
         File.expand_path("../less.js", __FILE__)
