84f3cf98f7be468466284c4860545a5a02257a91
Taylor Yelverton <taylor@synergy-solutions.biz>
2012-04-14 16:35:48 -0500

    Added less-rhino.js for java platform.

--- lib/less_js/source.rb
+++ lib/less_js/source.rb
@@ -1,7 +1,11 @@
 module LessJs
   module Source
     def self.bundled_path
-      File.expand_path("../less.js", __FILE__)
+      if RUBY_PLATFORM ~= /java/
+        File.expand_path("../less-rhino.js", __FILE__)
+      else
+        File.expand_path("../less.js", __FILE__)
+      end
     end
   end
 end
